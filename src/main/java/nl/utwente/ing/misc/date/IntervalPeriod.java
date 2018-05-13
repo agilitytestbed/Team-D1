c9aca60e440286358f9a3eb677304a3760913ed2
Daan Kooij <d.kooij-1@student.utwente.nl>
2018-05-13 14:10:59 +0200

    Create enum to specify interval periods.

---
+++ src/main/java/nl/utwente/ing/misc/date/IntervalPeriod.java
@@ -0,0 +1,13 @@
+package nl.utwente.ing.misc.date;
+
+/**
+ * The IntervalPeriod enum.
+ * Used to enumerate the possible interval sizes.
+ *
+ * @author Daan Kooij
+ */
+public enum IntervalPeriod {
+
+    HOUR, DAY, WEEK, MONTH, YEAR;
+
+}
